8792eaa4e714bada55f9a7170e4454fbbf8574b9
DataMonky <francis.ho2010@gmail.com>
2016-07-26 14:09:46 +1000

    Testing

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>Login Page</p>
 
 	</div>
-
+ 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.</p>
 </div>
 
