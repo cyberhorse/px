480a081788896ede3f1099ce8d7d1529039d15e7
Ivan Peevski <ivan@personifycare.com>
2016-07-26 12:30:33 +0930

    Update homepage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,23 +4,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Welcome to PX</title>
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+
+	<link href='css/bootstrap.min.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to Patient eXperience</h1>
+	<div class="panel">
+		<div class="panel-header">
+			<h1>Welcome to Patient eXperience</h1>
+		</div>
 
-	<div id="body">
-		<p>Login Page</p>
 
+		<div id="body" class="panel-body">
+			<p>Login Page</p>
+
+		</div>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.</p>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src='js/bootstrap.min.js'></script>
 
 </body>
 </html>
